4b139892ee8bffb48c95b143398acd2d23cc0095
sjkchang <sjkchang@gmail.com>
2021-01-22 20:03:43 -0800

    Combined all spotify related views into one file

--- app/views/spotify.py
+++ app/views/spotify.py
@@ -1,16 +1,18 @@
 from flask import current_app as app
-from flask import render_template, redirect, request, url_for
+from flask import redirect, request, url_for
 from flask_login import current_user
+from flask_login.utils import login_required
 
 import os
 import requests
-import urllib
-import base64
 from dotenv import load_dotenv
+
 
 from ..models.user_model import User
 from ..models.spotify_access_token_model import SpotifyAccessToken
 from .. import login_manager, db
+from ..utils import spotify
+
 
 load_dotenv()
 CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
@@ -18,7 +20,8 @@ CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
 BASE_URL = "https://accounts.spotify.com/authorize"
 
-
+### AUTHENTICATION
+#######################################################
 @app.route("/spotify/auth", methods=["GET", "POST"])
 def authenticate():
     URL = (
@@ -26,7 +29,7 @@ def authenticate():
         + f"?client_id={CLIENT_ID}"
         + "&response_type=code"
         + f"&redirect_uri={REDIRECT_URI}"
-        + "&scope=user-read-private"
+        + "&scope=user-read-private%20user-read-email%20user-library-read%20user-library-modify%20playlist-modify-public%20playlist-modify-private%20playlist-read-private"
     )
     print(URL)
     return redirect(URL)
@@ -58,12 +61,51 @@ def refresh():
     }
     response = requests.post(URL, data=body_params)
     data = response.json()
+    print(data)
     token = SpotifyAccessToken.create(
         data["access_token"],
         data["token_type"],
         data["scope"],
         data["expires_in"],
         data["refresh_token"],
-        current_user,
     )
+    current_user.add_spotify_token(token)
+
     return redirect(url_for("home"))
+
+
+### API Calls
+########################################################################
+
+
+@login_required
+@app.route("/test", methods=["GET", "POST"])
+def test():
+    token = current_user.spotify_token
+
+    access_token = token.access_token
+    user = spotify.getCurrentUser(access_token)
+    playlists = spotify.get_playlists(access_token, 50)
+    track = spotify.get_uri_for_track(
+        access_token, "This is Home", "Cavetown", "This is Home"
+    )
+    # new_playlist = spotify.create_playlist(
+    #  access_token, "Api test playlist", "Test", False
+    # )
+    add_song = spotify.add_track_to_playlist(
+        access_token, "375vOdUrkRS1nspEU0SDk7", "spotify:track:7s8VgA8OjvwBUuigKzEGBx"
+    )
+    return f"""
+            <h1>User</h1>
+            <p>{user}</p>
+            <h1>Playlists</h1>
+            <p>{playlists}</p>
+            <h1>Meet Virginia</h1>
+            <p>{track}</p>
+            <h1>New Playlist</h1>
+            <p></p>
+            <h1>new_track</h1>
+            <p>{add_song}</p>
+            <h1></h1>
+            <p></p>
+        """
