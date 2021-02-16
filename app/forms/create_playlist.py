595fb55325e1b1ec72ca152a843bb23015e9a899
sjkchang <sjkchang@gmail.com>
2021-02-15 20:11:45 -0800

    Added min and max sliders for audio features

--- app/forms/create_playlist.py
+++ app/forms/create_playlist.py
@@ -19,13 +19,36 @@ class CreatePlaylistForm(FlaskForm):
     private = BooleanField("Private")
     collaborative = BooleanField("Collaborative")
     genres = SelectField("genres", choices=[])
+    advanced = BooleanField("advanced")
+
     danceability = DecimalRangeField("Danceability", default=0)
+    min_danceability = DecimalRangeField("Danceability", default=0)
+    max_danceability = DecimalRangeField("Danceability", default=0)
+
     valence = DecimalRangeField("Valence", default=0)
+    min_valence = DecimalRangeField("Valence", default=0)
+    max_valence = DecimalRangeField("Valence", default=0)
+
     acousticness = DecimalRangeField("Acousticness", default=0)
+    min_acousticness = DecimalRangeField("Acousticness", default=0)
+    max_acousticness = DecimalRangeField("Acousticness", default=0)
+
     instrumentalness = DecimalRangeField("Instrumentalness", default=0)
+    min_instrumentalness = DecimalRangeField("Instrumentalness", default=0)
+    max_instrumentalness = DecimalRangeField("Instrumentalness", default=0)
+
     liveness = DecimalRangeField("Valence", default=0)
+    min_liveness = DecimalRangeField("Valence", default=0)
+    max_liveness = DecimalRangeField("Valence", default=0)
+
     energy = DecimalRangeField("Energy", default=0)
+    min_energy = DecimalRangeField("Energy", default=0)
+    max_energy = DecimalRangeField("Energy", default=0)
+
     speechiness = DecimalRangeField("Speechiness", default=0)
+    min_speechiness = DecimalRangeField("Speechiness", default=0)
+    max_speechiness = DecimalRangeField("Speechiness", default=0)
+
     loudness = DecimalRangeField("Loudness", default=-60)
 
     submit = SubmitField("Submit")
