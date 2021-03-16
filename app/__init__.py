72d0529ed30ffeb345aa706324e1b605bed3308a
sjkchang <sjkchang@gmail.com>
2021-03-15 18:45:10 -0700

    Removed test configs

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,9 @@
 from flask import Flask
 
 
-def create_app(testing):
+def create_app():
     app = Flask(__name__, instance_relative_config=False, template_folder="templates")
-    if not testing:
-        app.config.from_object("config.Config")
-    else:
-        app.config.from_object("config.TestConfig")
+    app.config.from_object("config.Config")
 
     with app.app_context():
         from .views import library, auth, nav
--- config.py
+++ config.py
@@ -1,10 +1,5 @@
+import os
+
+
 class Config:
-    SECRET_KEY = "b825c713da1c27fc72d8cb8d0875f7cc"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///seeker.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-class TestConfig:
-    SECRET_KEY = "b825c713da1c27fc72d8cb8d0875f7cc"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///seekerTest.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY")
